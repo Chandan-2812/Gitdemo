74c992e98dac334476cbf5040063c67749c651d0
Chandan-2812 <chandan.p@infosys.com>
2024-03-27 15:00:28 +0530

    pushed

---
+++ gitdemo/src/demo/test.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class test {
+
+	public static void main(String[] args) {
+		 int a =35;
+		 if (a%2==0)
+		 {
+			 System.out.println("even no");
+		 }
+		 else
+		 {
+			 System.out.println("odd no");
+		 }
+	}
+
+}
